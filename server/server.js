276f5b4e6fb396ce907b1198f0f589f45b121313
Edona Elshani <97137545+EdonaElshani@users.noreply.github.com>
2022-08-15 21:25:15 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./config/mongoose.config");
+//require("./routes/food.routes")(app);
+
+const server = app.listen(8000, () => {
+  console.log("Listening on port 8000");
+});
