7273eb5aae9cad35f91ea89270968ac8bd8985bd
Tringa Krasniqi <tringe.krs@gmail.com>
2022-08-20 16:54:06 +0200

    Edit Style

--- client/src/components/EditFood.js
+++ client/src/components/EditFood.js
@@ -1,16 +1,16 @@
 import axios from 'axios'
 import React, { useEffect , useState} from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import "../form.css"
 // import {FaPencilAlt} from "react-icons/fa"
 // import './EditPet.css';
 
-const EditPet = () => {
+const EditFood = () => {
     const [name, setName] = useState('');
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
     const [quantity, setQuantity] = useState('');
     const [calories, setCalories] = useState('');
-    const [imgPath, setImgPath] = useState('');
     const [price, setPrice] = useState(0);
     // const [errors, setErrors] = useState("");
     const {id} = useParams();
@@ -24,7 +24,6 @@ const EditPet = () => {
             setDescription(res.data.description)
             setQuantity(res.data.quantity)
             setCalories(res.data.calories)
-            setImgPath(res.data.imgPath)
             setPrice(res.data.price)
           })
           .catch((err) =>{
@@ -53,14 +52,12 @@ const EditPet = () => {
     }
   return (
     <div>
-        <div className='header'>
-            <h1>Food</h1>
-        </div>
-        <h3>Edit {name}</h3>
-        <form className='form' onSubmit={handleSubmit}>
-          <div className='form1' >
 
-            <label htmlFor='name'>Food Name:</label>
+        {/* <h3 classNameName="font-robo " >Edit {name}</h3>
+        <form classNameName=' row'  onSubmit={handleSubmit}>
+          <div classNameName='form1 row-space' >
+
+            <label htmlFor='name' classNameName="col-2">Name:</label>
             <input type='text' id='name' value={name} onChange={ e => setName(e.target.value)} />
            
             <label htmlFor='category'>Food Category:</label>
@@ -69,19 +66,76 @@ const EditPet = () => {
             <label htmlFor='description'>Description:</label>
             <input type='text' id='description' value={description} onChange={ e => setDescription(e.target.value)} />
 
-            <label htmlFor='quantity'>Quantity:</label>
+            <label htmlFor='quantity' >Quantity:</label>
             <input type='text' id='quantity' value={quantity} onChange={ e => setQuantity(e.target.value)} />
 
-            <label htmlFor='calories'>Calories:</label>
-            <input type='text' id='calories' value={calories} onChange={ e => setCalories(e.target.value)} />
+            <label htmlFor='calories' >Calories:</label>
+            <input type='text' id='calories'  value={calories} onChange={ e => setCalories(e.target.value)} />
 
             <label htmlFor='price'>Price:</label>
             <input type='number' id='price' value={price} onChange={ e => setPrice(e.target.value)} />
             <button>Update Movie</button>
           </div>
-        </form>
+        </form> */}
+        <body>
+    <div className="container page-wrapper bg-red p-t-180 p-b-100 mt-4 font-robo">
+        <div className="wrapper wrapper--w960">
+            <div className="card card-2">
+                <div className="card-heading"></div>
+                <div className="card-body">
+                    <h2 className="title">Edit Order</h2>
+                      <form method="POST" onSubmit={handleSubmit}>
+
+                        <div className="input-group">
+                            <input className="input--style-2" type="text" placeholder="Name" name="name" value={name} onChange={ e => setName(e.target.value)}/>
+                        </div>
+
+                        <div className="row row-space">
+                            <div className="col-2">
+                                <div className="input-group">
+                                    <div className="rs-select2 js-select-simple select--no-search">
+                                        <select name="category"  value={category} onChange={ e => setCategory(e.target.value)}>
+                                            <option disabled="disabled" selected="selected">Category</option>
+                                            <option>Breakfast</option>
+                                            <option>Lunch</option>
+                                            <option>Dinner</option>
+                                            <option>Dessert</option>
+                                            <option>Vegetarian</option>
+                                            <option>Vegan</option>
+                                        </select>
+                                        <div className="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="input-group">
+                            <input className="input--style-2" type="text" placeholder="Description" name="description" value={description} onChange={ e => setDescription(e.target.value)}/>
+                        </div>
+
+                        <div className="input-group">
+                            <input className="input--style-2" type="text" placeholder="Quantity" name="quantity" value={quantity} onChange={ e => setQuantity(e.target.value)}/>
+                        </div>
+
+                        <div className="input-group">
+                            <input className="input--style-2" type="text" placeholder="Calories" name="calories" value={calories} onChange={ e => setCalories(e.target.value)}/>
+                        </div>
+
+                        <div className="input-group">
+                            <input className="input--style-2" type="number" placeholder="Price" name="price" value={price} onChange={ e => setPrice(e.target.value)}/>
+                        </div>
+
+                        <div className="p-t-30">
+                            <button className="btn btn--green btn--green:hover" type="submit">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
     </div>
   )
 }
 
-export default EditPet
+export default EditFood
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import "../form.css"
 
 const Navbar = () => {
   return (
@@ -26,7 +27,7 @@ const Navbar = () => {
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn--green btn--green:hover" type="submit">Search</button>
       </form>
     </div>
   </div>
