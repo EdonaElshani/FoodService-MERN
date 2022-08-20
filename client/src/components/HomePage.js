99745c8f077ff385a4b930544f33d8a1754ebcdf
Tringa Krasniqi <tringe.krs@gmail.com>
2022-08-20 16:59:39 +0200

    homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,10 +3,12 @@ import Accordion from "./Accordion";
 import { HeroSection } from "./HeroSection";
 import HowItWorks from "./HowItWorks";
 import Testimonials from "./Testimonials";
+import DisplayAll from "./DisplayAll"
 
 const HomePage = () => {
   return (
     <div className="container">
+      <DisplayAll />
       <HeroSection />
       <HowItWorks/>
       <Testimonials />
