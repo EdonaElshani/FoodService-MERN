27ad24236abed98635837ea4de4c1237a1ca96c3
ArgjentaGashi <argjenta.gashi1@student.uni-pr.edu>
2022-08-20 12:46:23 +0200

    create display details delete

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Accordion from './Accordion'
+import DisplayAll from './DisplayAll'
 import Navbar from './Navbar'
 import Testimonials from './Testimonials'
 
 const HomePage = () => {
   return (
     <div>
+      <DisplayAll/>
       <Testimonials/>
       <Accordion/>
     </div>
