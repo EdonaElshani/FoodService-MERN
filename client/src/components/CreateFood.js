dce675150e98dc3f98d1db1fa115369543075a14
Edona Elshani <97137545+EdonaElshani@users.noreply.github.com>
2022-08-22 21:26:13 +0200

    Design Fix

--- client/src/components/CreateFood.js
+++ client/src/components/CreateFood.js
@@ -34,34 +34,6 @@ const CreateFood = () => {
 
   return (
     <div>
-    {/* <form
-      onSubmit={handleSubmit}
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-    >
-      <label>Name</label>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <label>Category</label>
-      <select value={category} name="category" onChange={(e) => setCategory(e.target.value)}>
-        <option>Select a Category</option>
-        <option value="Breakfast">Breakfast</option>
-        <option value="Lunch">Lunch</option>
-        <option value="Dinner">Dinner</option>
-        <option value="Dessert">Dessert</option>
-        <option value="Vegan">Vegan</option>
-        <option value="Vegetarian">Vegetarian</option>
-      </select>
-      <label>Description</label>
-      <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-      <label>Quantity</label>
-      <input type="text" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-      <label>Calories</label>
-      <input type="text" value={calories} onChange={(e) => setCalories(e.target.value)} />
-      <label>Price</label>
-      <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
-      <button>Add Food</button>
-    </form> */}
-
-
     <body>
         <div className="container page-wrapper bg-red p-t-180 p-b-100 mt-4 font-robo">
           <div className="wrapper wrapper--w960">
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,12 +3,10 @@ import Accordion from "./Accordion";
 import { HeroSection } from "./HeroSection";
 import HowItWorks from "./HowItWorks";
 import Testimonials from "./Testimonials";
-import DisplayAll from "./DisplayAll"
 
 const HomePage = () => {
   return (
     <div className="container">
-      {/* <DisplayAll /> */}
       <HeroSection />
       <HowItWorks/>
       <Testimonials />
